f08bfff92922a1a3bbd66c92ab8d11de76e75bbd
Ajay <imajvermaofficial@gmail.com>
2023-05-23 21:37:12 +0530

    adding a form

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './MealItem.module.css';
+import MealItemForm from "./MealItemFrom";
 
 const MealItem = (props) => {
 
@@ -12,7 +13,7 @@ const MealItem = (props) => {
             <div className={classes.price}>{price}</div>
         </div>
         <div>
-
+            <MealItemForm />
         </div>
     </li>
 
